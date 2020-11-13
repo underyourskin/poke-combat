2371c57af2b16050379144546765480eeb60f1f6
Daniel <leinadnehoc1@gmial.com>
2020-11-13 16:41:20 +0200

    New: components/UI-componentes/countdown.

---
+++ src/components/ui-components/countdown.js
@@ -0,0 +1,56 @@
+import * as PIXI from 'pixi.js';
+
+const STYLE = {
+  fill: "black",
+  fontFamily: "\"Comic Sans MS\", cursive, sans-serif",
+  fontSize: 80,
+  fontStyle: "italic",
+};
+
+export default class Countdown {
+  constructor(props = {
+    start: 5,
+    end: 1,
+    timeout: 1000,
+    x: 500,
+    y: 480
+  }) {
+    if (!props.stage) {
+      throw new Error('Stage is required prop');
+    }
+    this.props = props;
+  }
+
+  countdown(count) {
+    return new Promise(async (resolve) => {
+  
+      const style = new PIXI.TextStyle(STYLE);
+
+      const textObj = new PIXI.Text(` ${count} `, style);
+
+      const { x, y, stage, timeout } = this.props;
+
+      textObj.x = x;
+      textObj.y = y;
+      textObj.anchor.x = 0.5;
+
+      stage.addChild(textObj);
+
+      setTimeout(() => {
+        stage.removeChild(textObj);
+
+        // Give him a heartbeat 
+        setTimeout(() => {
+          resolve();
+        }, 100);
+      }, timeout)
+    });
+  }
+
+
+  async render() {
+    for (let i = this.props.start; i >= this.props.end; i--) {
+      await this.countdown(i);
+    }
+  }
+}
